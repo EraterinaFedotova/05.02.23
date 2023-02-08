8c1fc4be63cd7407b7f8e6c12698fb932a8de49a
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-08 22:23:42 +0300

    Начала решать 29 задачу

--- 29/Program.cs
+++ 29/Program.cs
@@ -4,3 +4,38 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+// int [] numbers = new int[8];
+// Console.Write("[");
+
+// for (int i = 0; i < numbers.Length; i++)
+//  {
+//     numbers [i] = new Random().Next(0, 11);
+//     Console.Write(" " + Method (i) + " ");
+//  }
+// Console.Write("]");
+
+// int Method (int a)
+// {
+//     return numbers[a];
+// }
+
+int N = 8;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+
+PrintArray(SetArray(N));
