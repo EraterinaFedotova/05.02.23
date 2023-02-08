5c7c0473480a03927db6ef656c6c57c6044c2247
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-08 21:28:45 +0300

    Fask 25

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,8 +1,25 @@
-﻿Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
-и возводит число A в натуральную степень B.
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
 
-3, 5 -> 243 (3⁵)
+// 3, 5 -> 243 (3⁵)
 
-2, 4 -> 16
+// 2, 4 -> 16
 
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число {a} в степени {b} = {NaturalDegree()}");
+
+
+int NaturalDegree() 
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result; 
+}
 
