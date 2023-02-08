b61c06af9a9cda5a54d7caffa21b0862b8eeb8b1
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-08 19:29:47 +0300

    Fask

---
+++ 24/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число А
+// и выдает сумму чисел от 1 до А.
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int n = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"Сумма чисел от 1 до {n} равна {GetSum (n)}");
+
+int GetSum (int limit) {
+int sum = 0;
+for (int i = 1; i <= limit; i++)
+sum += i;
+return sum;
+}
